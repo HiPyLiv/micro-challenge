9b5ad5e914f0a9f51befda484b0df8f866ad9ad9
treharne <R.Treharne@liverpool.ac.uk>
2016-08-22 14:17:38 +0100

    find prime tweak

--- EASY/find_prime/find_prime.py
+++ EASY/find_prime/find_prime.py
@@ -2,32 +2,27 @@ from numpy import random
 
 def random_list(N):
     '''
-    Function returns a list of random integers of length N 
-    with values in the range (0, 1e6)
+    Function returns a list of random integers of length N
     '''
-    random_list = [random.randint(0, 1e6) for r in range(N)]
-    return random_list
+    return [random.randint(0, 1e6) for r in range(N)]
 
-def prime_list(input_list):
+def is_prime(p):
     '''
-    Function finds all primes in a list of integers
-    and returns sublist containing primes
+    Checks to see whether a number is prime
+    Returns True or False
     '''
-    prime_list = []
-    for item in input_list:
-        if is_prime(item):
-            prime_list.append(item)
+    return all(p % i for i in range (2, p))
 
-    return prime_list
-
-def is_prime(a):
-    return all(a % i for i in range(2, a))
-    
-
-
+def find_max_prime(rList):
+    '''
+    Returns the largest prime number in a random list
+    of integers.
+    '''
+    # sort random list low --> high
+    sList = sorted(rList)
+    for el in sList[::-1]:
+        if is_prime(el):
+            return el
 
 if __name__ == "__main__":
-    rlist = random_list(1000)
-    plist = prime_list(rlist)
-    print(plist)
-
+    print(find_max_prime(random_list(1000)))
