1286b85d20e3dda2b2c0b36af16b80b96aea92a2
Sam Cassidy <sam@cassidys.email>
2016-08-23 10:06:57 +0100

    Corrected typo

--- EASY/palindrome_checker/palindrome_checker.py
+++ EASY/palindrome_checker/palindrome_checker.py
@@ -1,5 +1,5 @@
 def is_palindrome(istring):
-    '''Takes a string as input and returns a boolean
+   '''Takes a string as input and returns a boolean
    (True/False) output dependant on whether the
    string is a palindrome.
    '''
