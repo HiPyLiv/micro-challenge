a380a5e77faf571421fd341ff10dc3c31318cb65
Deaglan Crew <d.crew@liv.ac.uk>
2016-09-07 13:38:41 +0100

    Completed solutions

--- INTERMEDIATE/bacterial_growth/growth_analyser.py
+++ INTERMEDIATE/bacterial_growth/growth_analyser.py
@@ -5,8 +5,12 @@ series of metrics of bacterial growth. In this example I made use of
 a state table to make the analysis more adaptive, but there are other
 methods of doing this.
 '''
+import math
 
-states = []
+states = [("lag",lambda x,y: x == y),
+          ("log",lambda x,y: x > y),
+          ("max",lambda x,y: x == y),
+          ("death",lambda x,y: x < y)]
 
 def state_split (series, state_table):
     '''
@@ -14,32 +18,53 @@ def state_split (series, state_table):
     series according to boundary conditions within a state table,
     look at the solutions branch to better understand this component.
     '''
-    return
+    index = 1
+    output = {}
+    for state in states:
+        output[state[0]] = []
+        while state[1](series[index], series[index-1]) and index<len(series)-1:
+            index += 1
+            output[state[0]].append(series[index-1])
+    return output
 
-def growth_rate (log_series):
+def exponent_rate (log_series):
     '''
-    Bacteria grow exponentially, by log transforming the series and
+    Bacteria grow and die exponentially, by log transforming the series and
     measuring the average difference between each transform point,
-    you can get a unique measure of bacterial growth rate.
+    you can get a unique measure of bacterial growth and death rate.
     '''
-    return
+    trans_series = [math.log2(i) for i in log_series]
+    sum_of_diffs = 0
+    for i in range(len(trans_series)-1):
+        sum_of_diffs += trans_series[i+1]- trans_series[i]
+    return round(100*sum_of_diffs/(len(trans_series)-1))
 
-def death_rate (death_series):
-    '''
-    Similar to above, bacteria will die out once available resources
-    are exhausted, the rate at which they do so can be useful to know.
-    '''
-    return
-
-def bacterial_metrics (inputfile, outputfile):
+def bacterial_metrics (inputfile):
     '''
     Reads series and bacteria names from input file, and converts into
     discrete series.
     Uses series in table to determine the following metrics:-
     Time spent in lag phase,
     Growth rate constant,
-    Maximum bactrial number,
+    Maximum bacterial number,
     Death rate constant
-    Outputs these metrics as csv file.
+    Outputs these metrics as a list of dicts
     '''
-    return
+    output = []
+    with open(inputfile) as source_data:
+        for line in source_data:
+            if line[0] == "B":
+                bacteria = line.strip(" \n")
+            else:
+                series = [float(i) for i in line.strip(" \n").split(",")]
+                state_dict = state_split(series, states)
+                metrics = {"name": bacteria,
+                           "lag_length": len(state_dict["lag"]),
+                           "growth_rate": exponent_rate(state_dict["log"]),
+                           "max_number": max(state_dict["max"]),
+                           "death_rate": -1*exponent_rate(state_dict["death"])}
+                output.append(metrics)
+    return output
+
+for met in bacterial_metrics("bacseries.txt"):
+    print(met)
